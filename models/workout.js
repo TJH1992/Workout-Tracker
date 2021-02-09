a30b3ec4c88bf362a35ae8ae9265e3abfe425bf5
Emily Thomas <ethomas@quint.co.uk>
2021-02-08 22:59:42 -0800

    added workout schema

---
+++ models/workout.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model("workout", new mongoose.Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: {
+        type: Array,
+
+    }
+}));
